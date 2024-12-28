4e24f56570d32ba0f4f3606d4b9a6709f4f5c3eb
Rafael Gavarron <rafael.gavarron@gmail.com>
2024-12-28 22:04:47 +0000

    Python, neovim, git config e modificação de comentários

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-#!bin/bash
+#!/bin/bash
 
-sudo apt update ; sudo apt upgrade -y ; sudo apt distro-upgrade -y
+#Instalar Docker
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -27,10 +27,12 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 nvm install 22
 
 # verifica se a versão correta da Node.js está no ambiente
-node -v # deve imprimir `v22.11.0`
+node -v #deve imprimir `v22.11.0`
 
 #Instalar Git
-sudo apt install git-all
+sudo apt install git-all -y
+git config --global user.name "Rafael Gavarron"
+git config --global user.email rafael.gavarron@gmail.com
 
 #Instalar github
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
@@ -41,4 +43,22 @@ sudo apt install git-all
 	&& sudo apt update \
 	&& sudo apt install gh -y
 
-  #Instalar vim
+#Instalar nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+#Deixar no PATH
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+
+#Instalar Vagrant
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vagrant -y
+
+#Instalar ansible
+sudo apt install ansible -y
+
+#Configurar Python3 como Python
+sudo apt install python-is-python3 
